2881db4e9db4c33669ffd94e45a5926c184f5998
Raquel Ciusz <raquel.ciusz@yahoo.com>
2022-07-22 09:31:32 +0200

    loop

---
+++ Exercicios/aula13/ambiente.js
@@ -0,0 +1,6 @@
+
+var c = 1 //contador vai comecar em 1
+while (c <= 6) { //enquanto a variavel for menor ou igual a 6
+    console.log(`Passo ${c}`) //roda esse codigo
+    c++ // var c = c + 1 essa parte faz o numero da variavel mudar e ir aumentando 1 a cada vez que roda o bloco de codigo
+}
